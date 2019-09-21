57cc9abea28b0dc557b1056bb64f302340deef9d
segasle <segasle@gmail.com>
2019-09-21 14:10:38 +0300

    меню

--- template/header.php
+++ template/header.php
@@ -13,40 +13,39 @@
 <body>
 <header>
     <div class="row align-items-center">
-        <div class="col 12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
             <div class="logo">
                 <a href="/" class="logo-link"></a>
             </div>
         </div>
-        <div class="col 12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
             <div class="menu">
-               <!-- <input type="checkbox" id="menu">
-                <label for="menu" class="label-none">
-                    <i class=""></i>
-                </label>!-->
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="fas fa-align-justify fa-3x"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Disabled</a>
-                            </li>
+                          <?php
+
+                          $url = basename($_SERVER['REQUEST_URI']);
+                          global $mysqli;
+                          $sql = mysqli_query($mysqli,'SELECT * FROM `menu`');
+                          // $active = '';
+                          foreach ($sql as $r) {
+                              if ($r['link'] === $url) {
+                                  $active = 'active';
+                              } else {
+                                  $active = '';
+                              }
+                              echo "<li class='av-item " . $active . "'><a href='" . $r['link'] . "' class='nav-link'>" . $r['title'] . "</a></li>";
+                          }
+                          ?>
                         </ul>
                     </div>
                 </nav>
             </div>
         </div>
-        <div class="col 12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
+        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
     </div>
 </header>
