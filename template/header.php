8d4dfbd79aaae0eb0689d2a7e25a9410bf20f409
segasle <segasle@gmail.com>
2019-09-24 19:23:15 +0300

    header info

--- template/header.php
+++ template/header.php
@@ -15,12 +15,12 @@
 <body>
 <header class="header">
     <div class="row align-items-center">
-        <div class="col-8 col-sm-8 col-md-4 col-lg-2 col-xl-2 order-xs-2">
+        <div class="col-8 col-sm-8 col-md-4 col-lg-2 col-xl-1 order-xs-2">
             <div class="logo">
                 <a href="/" class="logo-link"></a>
             </div>
         </div>
-        <div class="col-4 col-sm-4 col-md-4 col-lg-5 col-xl-5 order-xs-1">
+        <div class="col-4 col-sm-4 col-md-4 col-lg-6 col-xl-5 order-xs-1">
             <div class="menu">
                 <nav class="navbar navbar-expand-lg navbar-light justify-content-flex-end">
                     <input type="checkbox" id="checkbox" class="d-none">
@@ -50,19 +50,25 @@
                 </nav>
             </div>
         </div>
-        <div class="col-12 col-sm-12 col-md-4 col-lg-5 col-xl-5 order-xs-3">
+        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-6 order-xs-3">
             <div class="info">
                 <div class="row">
-                    <div class="col-12 col-sm-12 col-md-6">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-5">
                         <a href="https://api.whatsapp.com/send?phone=79153301314" class="btn btn-md btn-pink"
                            target="_blank">
                             <i class="fab fa-whatsapp" aria-hidden="true"></i><span>Написать в WhatsÂpp</span>
                         </a>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
                         <a href="viber://chat?number=79153301314" class="btn btn-md btn-pink"
                            target="_blank">
                             <i class="fab fa-viber"></i><span>Написать в Viber</span>
+                        </a>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                        <a href="mailto:segasle@ya.ru" class="btn btn-md btn-pink"
+                           target="_blank">
+                            <i class="fab fa-mail-bulk"></i><span>Написать на почту</span>
                         </a>
                     </div>
                 </div>
