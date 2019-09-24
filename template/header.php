2108fe8cc3382bea4a7c136e66261c17d0baf203
segasle <segasle@gmail.com>
2019-09-24 10:44:44 +0300

    лого

--- template/header.php
+++ template/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Title</title>
+
+    <link rel="icon" href="/images/logo-light.png" type="image/png" sizes="<16>X<16>">
     <link rel="stylesheet" href="/icons/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
