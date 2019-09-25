1b086dc58022b2cd93cf18365bd9a68b2eafca74
sergejslepenkov <Segasle@gmail.com>
2019-09-25 15:17:13 +0300

    подвал

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 
 $(function() {
-    // $("body").css({padding:0,margin:0});
-    // var f = function() {
-    //     $(".ndra-container").css({position:"relative"});
-    //     var h1 = $("body").height();
-    //     var h2 = $(window).height();
-    //     var d = h2 - h1;
-    //     var h = $(".ndra-container").height() + d;
-    //     var ruler = $("<div>").appendTo(".ndra-container");
-    //     h = Math.max(ruler.position().top,h);
-    //     ruler.remove();
-    //     $(".ndra-container").height(h);
-    // };
-    // setInterval(f,1000);
-    // $(window).resize(f);
-    // f();
+    $("body").css({padding:0,margin:0});
+    var f = function() {
+        $(".ndra-container").css({position:"relative"});
+        var h1 = $("body").height();
+        var h2 = $(window).height();
+        var d = h2 - h1;
+        var h = $(".ndra-container").height() + d;
+        var ruler = $("<div>").appendTo(".ndra-container");
+        h = Math.max(ruler.position().top,h);
+        ruler.remove();
+        $(".ndra-container").height(h);
+    };
+    setInterval(f,1000);
+    $(window).resize(f);
+    f();
 
     $('#btnMenu').on('click', function(e) {
         e.preventDefault;
--- template/header.php
+++ template/header.php
@@ -14,66 +14,68 @@
 </head>
 <body>
 <div class="wrapper">
-<header class="header">
-    <div class="row align-items-center">
-        <div class="col-5 col-sm-6 col-md-4 col-lg-2 col-xl-1 order-xs-2">
-            <div class="logo">
-                <a href="/" class="logo-link"></a>
+    <header class="header">
+        <div class="row align-items-center">
+            <div class="col-5 col-sm-6 col-md-4 col-lg-2 col-xl-1 order-xs-2">
+                <div class="logo">
+                    <a href="/" class="logo-link"></a>
+                </div>
             </div>
-        </div>
-        <div class="col-7 col-sm-6 col-md-4 col-lg-6 col-xl-5 order-xs-1">
-            <div class="menu">
-                <nav class="navbar navbar-expand-lg navbar-light justify-content-flex-end">
-                    <input type="checkbox" id="checkbox" class="d-none">
-                    <label for="checkbox" class="label-none burger">
-                        <span class="fas fa-align-justify fa-3x" id="btnMenu"></span>
-                    </label>
-                    <div class="collapse navbar-collapse bg-light" id="navbarNav">
-                        <ul class="navbar-nav justify-content-space-between w-100">
-                            <?php
+            <div class="col-7 col-sm-6 col-md-4 col-lg-6 col-xl-5 order-xs-1">
+                <div class="menu">
+                    <nav class="navbar navbar-expand-lg navbar-light justify-content-flex-end">
+                        <input type="checkbox" id="checkbox" class="d-none">
+                        <label for="checkbox" class="label-none burger">
+                            <span class="fas fa-align-justify fa-3x" id="btnMenu"></span>
+                        </label>
+                        <div class="collapse navbar-collapse bg-light" id="navbarNav">
+                            <ul class="navbar-nav justify-content-space-between w-100">
+                                <?php
 
-                            $url = basename($_SERVER['REQUEST_URI']);
-                            //echo $url;
-                            global $mysqli;
-                            $sql = mysqli_query($mysqli, 'SELECT * FROM `menu`');
-                            // $active = '';
-                            foreach ($sql as $r) {
-                                if ($r['link'] === $url) {
-                                    $active = 'active';
-                                } else {
-                                    $active = '';
+                                $url = basename($_SERVER['REQUEST_URI']);
+                                //echo $url;
+                                global $mysqli;
+                                $sql = mysqli_query($mysqli, 'SELECT * FROM `menu`');
+                                // $active = '';
+                                foreach ($sql as $r) {
+                                    if ($r['link'] === $url) {
+                                        $active = 'active';
+                                    } else {
+                                        $active = '';
+                                    }
+                                    echo "<li class='nav-item " . $active . "'><a href='" . $r['link'] . "' class='nav-link'>" . $r['title'] . "</a></li>";
                                 }
-                                echo "<li class='nav-item " . $active . "'><a href='" . $r['link'] . "' class='nav-link'>" . $r['title'] . "</a></li>";
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                </nav>
+                                ?>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
-        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-6 order-xs-3">
-            <div class="info">
-                <div class="row align-items-center">
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-5">
-                        <a href="https://api.whatsapp.com/send?phone=79153301314" class="btn btn-md btn-pink btn-block"
-                           target="_blank">
-                            <i class="fab fa-whatsapp" aria-hidden="true"></i><span>Написать в WhatsÂpp</span>
-                        </a>
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                        <a href="viber://chat?number=79153301314" class="btn btn-md btn-pink btn-block"
-                           target="_blank">
-                            <i class="fab fa-viber"></i><span>Написать в Viber</span>
-                        </a>
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
-                        <a href="mailto:segasle@ya.ru" class="btn btn-md btn-pink btn-block"
-                           target="_blank">
-                            <i class="fas fa-envelope"></i><span>Написать на почту</span>
-                        </a>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-6 order-xs-3">
+                <div class="info">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-5">
+                            <a href="https://api.whatsapp.com/send?phone=79153301314"
+                               class="btn btn-md btn-pink btn-block"
+                               target="_blank">
+                                <i class="fab fa-whatsapp" aria-hidden="true"></i><span>Написать в WhatsÂpp</span>
+                            </a>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                            <a href="viber://chat?number=79153301314" class="btn btn-md btn-pink btn-block"
+                               target="_blank">
+                                <i class="fab fa-viber"></i><span>Написать в Viber</span>
+                            </a>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+                            <a href="mailto:segasle@ya.ru" class="btn btn-md btn-pink btn-block"
+                               target="_blank">
+                                <i class="fas fa-envelope"></i><span>Написать на почту</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
+    <main class="content ndra-container">
