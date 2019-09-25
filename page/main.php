2e0a35aa79ddfecf97d7df043e474bbf53420a65
sergejslepenkov <Segasle@gmail.com>
2019-09-25 18:21:57 +0300

    форма

--- page/main.php
+++ page/main.php
@@ -1,70 +1,52 @@
 <div class="row">
-    <div class="col-12 col-sm-12 col-md-6">igtyujhnjmkjn</div>
+    <div class="col-12 col-sm-12 col-md-6"></div>
     <div class="col-12 col-sm-12 col-md-6">
-        <form>
+        <form method="post" enctype="multipart/form-data">
             <div class="form-row">
-                <div class="col-md-4 mb-3">
+                <div class="col-12 col-md-4 mb-3">
                     <label for="validationServer01">Имя</label>
-                    <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Имя" value="Mark" required>
+                    <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Имя"
+                           required="required">
                     <div class="valid-feedback">
                         Looks good!
                     </div>
                 </div>
-                <div class="col-md-4 mb-3">
-                    <label for="validationServer02">Фамилия</label>
-                    <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Фамилия" value="Otto" required>
+                <div class="col-12 col-md-8 mb-3">
+                    <label for="validationServer02">телефон</label>
+                    <input type="text" class="form-control phone-number is-valid" id="validationServer02"
+                           required="required">
                     <div class="valid-feedback">
                         Looks good!
-                    </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <label for="validationServerUsername">Имя пользователя</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroupPrepend3">@</span>
-                        </div>
-                        <input type="text" class="form-control is-invalid" id="validationServerUsername" placeholder="Имя пользователя" aria-describedby="inputGroupPrepend3" required>
-                        <div class="invalid-feedback">
-                            Please choose a username.
-                        </div>
                     </div>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col-md-6 mb-3">
                     <label for="validationServer03">City</label>
-                    <input type="text" class="form-control is-invalid" id="validationServer03" placeholder="City" required>
+                    <input type="text" class="form-control is-invalid" id="validationServer03" placeholder="City"
+                           required>
                     <div class="invalid-feedback">
                         Please provide a valid city.
                     </div>
                 </div>
                 <div class="col-md-3 mb-3">
                     <label for="validationServer04">State</label>
-                    <input type="text" class="form-control is-invalid" id="validationServer04" placeholder="State" required>
+                    <input type="text" class="form-control is-invalid" id="validationServer04" placeholder="State"
+                           required>
                     <div class="invalid-feedback">
                         Please provide a valid state.
                     </div>
                 </div>
                 <div class="col-md-3 mb-3">
                     <label for="validationServer05">Zip</label>
-                    <input type="text" class="form-control is-invalid" id="validationServer05" placeholder="Zip" required>
+                    <input type="text" class="form-control is-invalid" id="validationServer05" placeholder="Zip"
+                           required>
                     <div class="invalid-feedback">
                         Please provide a valid zip.
                     </div>
                 </div>
             </div>
-            <div class="form-group">
-                <div class="form-check">
-                    <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
-                    <label class="form-check-label" for="invalidCheck3">
-                        Agree to terms and conditions
-                    </label>
-                    <div class="invalid-feedback">
-                        You must agree before submitting.
-                    </div>
-                </div>
-            </div>
-            <button class="btn btn-primary" type="submit">Submit form</button>
+            <button class="btn btn-pink" type="submit">Отправить</button>
         </form>
     </div>
 </div>
