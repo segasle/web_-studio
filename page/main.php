fd24fd639e7a8846a444d9b276f3d5f1a1121712
sergejslepenkov <Segasle@gmail.com>
2019-09-26 10:05:01 +0300

    форма

--- page/main.php
+++ page/main.php
@@ -7,6 +7,7 @@
                 <div class="col-12 col-md-4 mb-3">
                     <label for="validationServer01">Имя</label>
                     <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Имя"
+                           name="name"
                            required="required">
                     <div class="valid-feedback">
                         Looks good!
@@ -14,16 +15,16 @@
                 </div>
                 <div class="col-12 col-md-4 mb-3">
                     <label for="validationServer02">Телефон</label>
-                    <input type="text" class="form-control phone-number is-valid" placeholder="+7(999)999-99-99" id="validationServer02"
-                           required="required">
+                    <input type="text" class="form-control phone-number is-valid" placeholder="+7(999)999-99-99"
+                           name="phone" id="validationServer02">
                     <div class="valid-feedback">
                         Looks good!
                     </div>
                 </div>
                 <div class="col-12 col-md-4 mb-3">
                     <label for="validationServer02">Email</label>
-                    <input type="email" class="form-control is-valid" placeholder="sega@ya.ru" id="validationServer02"
-                           required="required">
+                    <input type="email" class="form-control is-valid" placeholder="sega@ya.ru" name="email"
+                           id="validationServer02">
                     <div class="valid-feedback">
                         Looks good!
                     </div>
@@ -32,18 +33,18 @@
 
             <div class="form-group">
                 <label for="exampleInputPassword1">Тема</label>
-                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Тема">
+                <input type="text" class="form-control" id="exampleInputPassword1" name="topiс" placeholder="Тема">
             </div>
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">Сообщение</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea1" name="mess" rows="3"></textarea>
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlFile1">Файл</label>
-                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                <input type="file" class="form-control-file" name="file" id="exampleFormControlFile1">
             </div>
-            <button class="btn btn-pink" type="submit">Отправить</button>
+            <button class="btn btn-pink" name="submit" type="submit">Отправить</button>
         </form>
     </div>
 </div>
