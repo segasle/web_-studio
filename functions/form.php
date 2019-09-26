f4cab318fab9e1ab0fc93991126e8f944227d980
segasle <segasle@gmail.com>
2019-09-26 16:23:56 +0300

    форма

--- functions/form.php
+++ functions/form.php
@@ -26,6 +26,45 @@ if (isset($_POST['submit'])) {
         }
         if (!isset($file)){
             $res = mysqli("INSERT INTO `feedback`(`name`, `phone`, `email`, `topic`, `message`) VALUES ('{$name}','{$phone}','{$email}','{$topic}','{$message}')");
+     if ($res){
+         echo 'отправлено';
+     }
+        }else{
+
+            $update = $_SERVER['DOCUMENT_ROOT'] . '/download/';
+            $file = $_FILES['file']['name'];
+            $update_file = $update . $file;
+            if (!file_exists($update_file)) {
+                if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+                    if ($_FILES['file']['size'] > 1024 * 3 * 1024) {
+                        echo '<div class="errors">Файл должен быть не больше 3 МБ</div>';
+                    } else {
+                        $ext = pathinfo($update_file, PATHINFO_EXTENSION);
+                        $allow = array('jpeg', 'jpg', 'png', 'xlsx', 'docx');
+                        if (in_array($ext, $allow)) {
+                            if (move_uploaded_file($_FILES['file']['tmp_name'], $update_file)) {
+                                $nameto = 'Имя:' . $name . "<br>";
+                                $phoneto = 'Номер:' . $phone . "<br>";
+                                $emailto = 'Почта:' . $email . "<br>";
+                                $txtto = 'Сообщение:' . $data['content'] . "<br>";
+                                $mess = $nameto . $phoneto . $emailto . $txtto;
+                                $wer = mysqli("INSERT INTO `feedback` (`email`, `name`, `phone`, `text`, `dock`) VALUES ('{$email}','{$name}','{$phone}', '{$data['content']}','{$file}')");
+                                if (!empty($wer)) {
+                                    $to = 'jim-owner@yandex.ru';
+                                    $subject = 'обратная связь';
+                                    $message = "$mess";
+                                    $headers = 'From: segasle@kafe-lyi.ru' . "\r\n" .
+                                        'Reply-To: segasle@kafe-lyi.ru' . "\r\n" .
+                                        "Content-Type: text/html; charset=\"UTF-8\"\r\n"
+                                        . 'X-Mailer: PHP/' . phpversion();
+                                    $mail = mail("$to", "$subject", "$message", "$headers");
+                                    echo '<div class="go">Успешно подано</div>';
+                                } else {
+                                   // echo $mysqli->error;
+                                }
+                            }
+                        }
+                    }
         }
     }
 }
