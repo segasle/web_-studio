0345c37ea781d43e4b7586be227523fd3c70a3f4
segasle <segasle@gmail.com>
2019-09-29 12:52:22 +0300

    текст

--- page/main.php
+++ page/main.php
@@ -3,7 +3,7 @@
         <div class="col-12 col-sm-12 col-md-6">
             <h1 class="text-center">У нас нет преград</h1>
             <div class="block_text">
-                <p class="text">Мы команда профессионалов, которая основана над улучшением IT технологими</p>
+                <p class="text h4">Мы - команда профессиональных WEB-разработчиков, создаем новое, улучшаем старое.</p>
             </div>
         </div>
         <div class="col-12 col-sm-12 col-md-6">
