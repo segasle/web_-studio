2cc7a5ba41bbeca020888d74ef72571370fe224b
sergejslepenkov <Segasle@gmail.com>
2019-10-05 21:14:51 +0300

    отзывы

--- page/comments.php
+++ page/comments.php
@@ -1,6 +1,29 @@
 <div class="wrap">
-    <div class="container">
+    <div class="container-fluid">
         <h1 class="text-center">Наши отзывы</h1>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <p class="h2">Оставьте отзыв</p>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="validationServer01" class="col-form-label-lg">Имя</label>
+                        <input type="text" class="form-control-lg form-control name animation-bs border"
+                               id="validationServer01"
+                               placeholder="Имя" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1" class="col-form-label-lg">Сообщение</label>
+                        <textarea class="form-control message form-control-lg animation-bs border"
+                                  id="exampleFormControlTextarea1"
+                                  name="message"
+                                  rows="3"></textarea>
+                    </div>
+                </form>
+                <button class="btn btn-pink btn-lg" name="submit" type="submit" id="ajaxBut">Отправить</button>
+
+            </div>
+            <div class="col-12 col-md-6"></div>
+        </div>
 
     </div>
 </div>
