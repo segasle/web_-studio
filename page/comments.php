7e0116281da39bba103e7b646b3a7ed0252e871e
sergejslepenkov <Segasle@gmail.com>
2019-10-06 16:07:46 +0300

    отзывы

--- page/comments.php
+++ page/comments.php
@@ -24,16 +24,35 @@
             </div>
             <div class="col-12 col-md-6">
                 <?php
-                    $sql = mysqli('SELECT * FROM `comments`');
-                    if (mysqli_fetch_row($sql) > 0){
-                        foreach ($sql as $item){
-                ?>
-
-                            <?php
-                        }
-                    }else{
-                        echo '<p class="h2">нет отзывов</p>';
+                $sql = mysqli('SELECT * FROM `comments`');
+                if (mysqli_fetch_row($sql) > 0) {
+                    foreach ($sql as $item) {
+                        ?>
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="..." alt="Первый слайд">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                        <?php
                     }
+                } else {
+                    echo '<p class="h2">нет отзывов</p>';
+                }
                 ?>
             </div>
         </div>
