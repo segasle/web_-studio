94802a51bb4f1959ee079f66cfd47723dac7e737
segasle <segasle@gmail.com>
2019-10-08 17:16:00 +0300

    контакты

--- page/сontacts.php
+++ page/сontacts.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sergejslepenkov
- * Date: 2019-09-29
- * Time: 13:43
- */
+<div class="wrap">
+    <h1 class="text-center">Контакты</h1>
+    <div class="container">
+        <p class="h3 bold">Руководитель</p>
+        <?php
+            $sql = mysqli('SELECT * FROM `contacts`');
+            $array = mysqli_fetch_array($sql);
+            //print_r($array);
+
+        ?>
+        <p><b class="h5 bold">ФИО:</b><i><?php echo $array['fio']; ?></i></p>
+
+    </div>
+</div>
