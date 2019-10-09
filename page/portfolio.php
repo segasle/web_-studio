af0a2783d65dcbe41b6d355f67b462e257c4dc23
segasle <segasle@gmail.com>
2019-10-09 15:49:59 +0300

    портфолио

--- page/portfolio.php
+++ page/portfolio.php
@@ -4,14 +4,19 @@
         <div class="row">
             <div class="col-12 col-sm-12 col-md-4 col-lg-2 col-xl-1">
                 <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"
-                       aria-controls="v-pills-home" aria-selected="true">Home</a>
-                    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"
-                       aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-                    <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab"
-                       aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-                    <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
-                       aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                    <?php
+                    $data = mysqli('SELECT * FROM `services`');
+                    foreach ($data as $item) {
+                        $id = $item['id'];
+                        ?>
+                        <a class="nav-link <?php echo $item['active']; ?>" id="btn-<?php echo $id; ?>-tab"
+                           data-toggle="pill" href="#btn-<?php echo $id; ?>" role="tab"
+                           aria-controls="btn-<?php echo $id; ?>"
+                           aria-selected="<?php echo $item['aria-selected']; ?>"><?php echo $item['title']; ?></a>
+                        <?php
+                    }
+                    ?>
+
                 </div>
             </div>
             <div class="col-12 col-sm-12 col-md-8 col-lg-10 col-xl-11">
@@ -24,10 +29,3 @@
         </div>
     </div>
 </div>
-<?php
-/**
- * Created by PhpStorm.
- * User: sergejslepenkov
- * Date: 2019-09-29
- * Time: 13:41
- */
