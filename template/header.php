fcefd5fd7cf24d6f627c459efe6d48db8b659168
segasle <segasle@gmail.com>
2019-10-20 11:05:36 +0300

    favicon

--- template/header.php
+++ template/header.php
@@ -25,8 +25,8 @@ $array = mysqli_fetch_array($sql);
     <meta name="description" content="Мы - команда профессиональных WEB-разработчиков, создаем новое, улучшаем старое.">
     <title>Веб студия</title>
 
-    
-    <link rel="icon" href="/images/logo-light.png" type="image/png" sizes="<16>X<16>">
+
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/icons/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
