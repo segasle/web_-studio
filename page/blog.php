cf68ff2f78f7d8f57dd6bd54e81668e4685216c0
segasle <segasle@gmail.com>
2019-12-04 11:34:22 +0300

    блог

--- page/blog.php
+++ page/blog.php
@@ -1,29 +1,16 @@
 <h1 class="text-center">Наш блог</h1>
-<div class="card-deck">
-    <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Название карточки</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="btn btn-md btn-pink btn-block"><small class="">Last updated 3 mins ago</small></p>
+<div class="row">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+        <div class="card">
+            <img src="/images/belka.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title h3">Название карточки</h5>
+                <p class="card-text text-justify">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <a class="btn btn-md btn-pink btn-block"><small class="">Last updated 3 mins ago</small></a>
+            </div>
         </div>
     </div>
-    <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Название карточки</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <p class="btn btn-md btn-pink btn-block"><small>Last updated 3 mins ago</small></p>
-        </div>
-    </div>
-    <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Название карточки</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <p class="btn btn-md btn-pink btn-block"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-    </div>
+
 </div>
 <?php
 /**
