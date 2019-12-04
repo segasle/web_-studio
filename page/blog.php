c062278908fac4091f7aa8ac1c4279a1627b45e6
sergejslepenkov <Segasle@gmail.com>
2019-12-04 12:57:47 +0300

    адаптив сетка

--- page/blog.php
+++ page/blog.php
@@ -1,12 +1,15 @@
 <h1 class="text-center">Наш блог</h1>
 <div class="row">
-    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xll-2">
         <div class="card">
             <img src="/images/belka.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title h3">Название карточки</h5>
-                <p class="card-text text-justify">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <a class="btn btn-md btn-pink btn-block"><small class="">Last updated 3 mins ago</small></a>
+                <p class="card-text text-justify">This is a longer card with supporting text below as a natural lead-in
+                    to additional content. This content is a little bit longer.</p>
+                <a href="#" class="btn btn-md btn-pink btn-block">
+                    <small class="">Last updated 3 mins ago</small>
+                </a>
             </div>
         </div>
     </div>
