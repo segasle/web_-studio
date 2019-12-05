893bffccd865aa125dc05dc29e3c4c05ef6ad3ed
segasle <segasle@gmail.com>
2019-12-05 18:09:44 +0300

    блог

--- page/blog.php
+++ page/blog.php
@@ -1,21 +1,23 @@
 <h1 class="text-center">Наш блог</h1>
 <div class="row">
-    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
-        <div class="card">
-            <img src="/images/belka.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title h3">Название карточки</h5>
-                <p class="card-text text-justify">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <a class="btn btn-md btn-pink btn-block"><small class="">Last updated 3 mins ago</small></a>
+    <?php
+    $data = mysqli('SELECT * FROM `thumbnails`');
+    foreach ($data as $item) {
+        ?>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+            <div class="card">
+                <img src="/images/thumbnails/<?php echo $item['photo'];?>" class="card-img-top" alt="<?php echo $item['head'];?>">
+                <div class="card-body">
+                    <h5 class="card-title h3"><?php echo $item['head'];?></h5>
+                    <p class="card-text text-justify"><?php echo $item['descriptions'];?></p>
+                    <a href="<?php echo $item['link'];?>" class="btn btn-md btn-pink btn-block">
+                        <small class="">Подробнее</small>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
+        <?php
+
+    } ?>
 
 </div>
-<?php
-/**
- * Created by PhpStorm.
- * User: sergejslepenkov
- * Date: 2019-12-03
- * Time: 10:50
- */
