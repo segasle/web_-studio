2a2096ed0a871f2505b69298d391fcbf12960f45
sergejslepenkov <Segasle@gmail.com>
2019-12-06 18:06:41 +0300

    блог

---
+++ page/Smarttv.php
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergejslepenkov
+ * Date: 2019-12-06
+ * Time: 17:52
+ */
+postarticles();
---
+++ page/game.php
@@ -0,0 +1,9 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergejslepenkov
+ * Date: 2019-12-06
+ * Time: 17:36
+ */
+postarticles();
