15fa9236814f1143dc9a225034dc91381c97c865
segasle <segasle@gmail.com>
2020-01-09 10:11:47 +0300

    форум

--- page/hat.php
+++ page/hat.php
@@ -1,5 +1,6 @@
 <h1 class="text-center">Форум</h1>
-<div class="container">                <form method="post" class="comment-form">
+<div class="container">
+    <form method="post" class="comment-form">
         <div class="form-group">
             <label for="validationServer01" class="col-form-label-lg">Имя</label>
             <input type="text" class="form-control-lg form-control name animation-bs border"
@@ -17,7 +18,86 @@
         </div>
     </form>
     <button class="btn btn-pink btn-lg" name="submit" type="submit" id="ajaxComm">Отправить</button>
-    <div class="block border">
+    <?php
+    $sql = mysqli("SELECT * FROM `hat`");
+    foreach ($sql as $item) {
+        ?>
+        <div class="block border">
+            <div class="block_head">
+                <div class="row">
+                    <div class="col">
+                        <p><i class="fas fa-user-circle fa-2x"></i><i
+                                    class="head-text"><?php echo $item['name']; ?></i></p>
+                    </div>
+                    <div class="col">
+                        <div class="float-right">
+                            <p><i class="fas fa-clock fa-2x"></i><b
+                                        class="head-text bold"><?php echo $item['date']; ?></b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="float-right">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="fas fa-share fa-2x" data-toggle="modal"
+                                    data-target="#exampleModal<?php echo $item['id']; ?> ">
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal<?php echo $item['id']; ?>" tabindex="-1" role="dialog"
+                                 aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Ответить на сообщение</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+
+                                            <form method="post" class="comment-form">
+                                                <div class="form-group">
+                                                    <label for="validationServer01"
+                                                           class="col-form-label-lg">Имя</label>
+                                                    <input type="text"
+                                                           class="form-control-lg form-control name animation-bs border"
+                                                           id="validationServer01"
+                                                           placeholder="Имя" name="name">
+                                                    <div id="ansName"></div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleFormControlTextarea1" class="col-form-label-lg">Сообщение</label>
+                                                    <textarea
+                                                            class="form-control message form-control-lg animation-bs border"
+                                                            id="exampleFormControlTextarea1"
+                                                            name="message"
+                                                            rows="3"></textarea>
+                                                    <div id="ansMessage"></div>
+                                                </div>
+                                            </form>
+                                            <button class="btn btn-pink btn-lg" name="submit" type="submit"
+                                                    id="ajaxComm">
+                                                Отправить
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="block_container border-top border-bottom">
+                <div class="content">
+                    <p class="text"><?php echo $item['sms']; ?></p>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="block border" style="margin-left: 40px;">
         <div class="block_head">
             <div class="row">
                 <div class="col">
@@ -33,7 +113,52 @@
                 </div>
                 <div class="col">
                     <div class="float-right">
-                        <i class="fas fa-share fa-2x" title="Ответить" style="cursor: pointer;"></i>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="fas fa-share fa-2x" data-toggle="modal"
+                                data-target="#exampleModal">
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                             aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Ответить на сообщение</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+
+                                        <form method="post" class="comment-form">
+                                            <div class="form-group">
+                                                <label for="validationServer01"
+                                                       class="col-form-label-lg">Имя</label>
+                                                <input type="text"
+                                                       class="form-control-lg form-control name animation-bs border"
+                                                       id="validationServer01"
+                                                       placeholder="Имя" name="name">
+                                                <div id="ansName"></div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleFormControlTextarea1" class="col-form-label-lg">Сообщение</label>
+                                                <textarea
+                                                        class="form-control message form-control-lg animation-bs border"
+                                                        id="exampleFormControlTextarea1"
+                                                        name="message"
+                                                        rows="3"></textarea>
+                                                <div id="ansMessage"></div>
+                                            </div>
+                                        </form>
+                                        <button class="btn btn-pink btn-lg" name="submit" type="submit"
+                                                id="ajaxComm">
+                                            Отправить
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -44,5 +169,30 @@
             </div>
         </div>
     </div>
+    <nav aria-label="Page navigation example" style="
+    margin-top: 50px;">
+        <ul class="pagination justify-content-center pagination-lg">
+            <li class="page-item disabled">
+                <span class="page-link"><i class="fas fa-angle-double-left"></i></span>
+            </li>
+            <li class="page-item disabled">
+                <span class="page-link"><i class="fas fa-angle-left"></i></span>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item active">
+      <span class="page-link">
+        2
+        <span class="sr-only">(current)</span>
+      </span>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+            <li class="page-item">
+                <a class="page-link" href="#"><i class="fas fa-angle-right"></i></a>
+            </li>
+            <a class="page-link" href="#"><i class="fas fa-angle-double-right"></i></a>
+            </li>
+        </ul>
+    </nav>
 </div>
 <?php
